431e74ebd0e897c4288273570744c0987aa12182
GerilsonAmazonas <gaa.lic23@uea.edu.br>
2025-07-25 20:32:46 -0400

    final

---
+++ README.md
@@ -0,0 +1,19 @@
+# c_sharp
+
+projeto: c_sharp
+
+## instalação
+
+instalação obrigatória:
+1. Visual Studio Code
+    - C# dev kit
+    - .NET Install Tool
+2. .NetCore 9.0
+
+## Como rodar
+
+dê Run no arquivo program.cs (f5 ou botão de play),
+ irá apracer no seu navegador como localhost
+
+
+
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328dc752d0b1ac5c29125cd4275c6b4ca9199e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9217d7f4ff083c7b4bbf66fc1f41951b7dd3c680")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
